3a20a3e8312eb1514ce363574f3d8ffdb71f9c10
Jeffrey Baumes <jeff.baumes@kitware.com>
2024-10-26 20:03:49 -0400

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,20 @@
+project = "nmdc-client"
+copyright = "2024, NMDC Collaborators"
+author = "NMDC Collaborators"
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# Add the favicon
+html_favicon = "_static/favicon.ico"
---
+++ docs/source/index.rst
@@ -0,0 +1,11 @@
+nmdc-client Documentation
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: nmdc_client
+   :members:
+   :undoc-members:
+   :show-inheritance:
